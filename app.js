d15bb1d6afcf3a37f8499770ba677b2937624fa4
lancelot1337 <sudhanshusah2001@gmail.com>
2018-07-22 21:54:23 +0530

    Add bodyParser and handle CORS errors

--- app.js
+++ app.js
@@ -3,9 +3,27 @@ const app = express();
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders')
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 //for logging
 app.use(morgan('dev'));
+
+//for bodyParser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//to handle cross-origin resource sharing (CORS) errors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    
+    //browser send an OPTIONS request when we send post or put request
+    if (req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods','PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
 
 //routes to productRoutes
 app.use('/products', productRoutes);
