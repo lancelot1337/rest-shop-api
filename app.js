b666c719fb2ad8958d8ed72df887317434b2cac2
lancelot1337 <sudhanshusah2001@gmail.com>
2018-07-22 21:36:47 +0530

    Add default logging and error handling

--- app.js
+++ app.js
@@ -2,8 +2,29 @@ const express = require('express');
 const app = express();
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders')
+const morgan = require('morgan');
+
+//for logging
+app.use(morgan('dev'));
 
 //routes to productRoutes
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+//for error handling
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
+
 module.exports = app;
