94c9918feb9e83909c6314fb64153db7eebbfdb3
lancelot1337 <sudhanshusah2001@gmail.com>
2018-07-23 13:45:58 +0530

    Fixes "can't connect to cloud db #1"

--- app.js
+++ app.js
@@ -7,18 +7,18 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-// //for mongoose
-// mongoose.connect('mongodb://node-rest-shop:' + process.env.MONGODB_PWD + '@node-rest-shop-shard-00-00-4b5cx.mongodb.net:27017,node-rest-shop-shard-00-01-4b5cx.mongodb.net:27017,node-rest-shop-shard-00-02-4b5cx.mongodb.net:27017/test?ssl=true&replicaSet=node-rest-shop-shard-0&authSource=admin&retryWrites=true', {
-//     useMongoClient: true
-// })
-
-mongoose.connect('mongodb://localhost/node-rest-shop').then(()=>{
-	console.log('db connect');
-},(e)=>{
-	console.log('error:'+ e)
+//for mongoose
+mongoose.connect('mongodb://node-rest-shop:' + process.env.MONGODB_PWD + '@node-rest-shop-shard-00-00-4b5cx.mongodb.net:27017,node-rest-shop-shard-00-01-4b5cx.mongodb.net:27017,node-rest-shop-shard-00-02-4b5cx.mongodb.net:27017/test?ssl=true&replicaSet=node-rest-shop-shard-0&authSource=admin&retryWrites=true', {
+    useMongoClient: true
 })
 
-//mongoose.Promise = global.Promise;
+// mongoose.connect('mongodb://localhost/node-rest-shop').then(()=>{
+// 	console.log('db connect');
+// },(e)=>{
+// 	console.log('error:'+ e)
+// })
+
+mongoose.Promise = global.Promise;
 
 //for logging
 app.use(morgan('dev'));
